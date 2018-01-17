7b92a938acecbcf4befae2a2bd6c72a7711cd298
hp04301986 <hp393747035@126.com>
2018-01-17 16:58:05 +0800

    init commit

--- 002addtwonumbers.py
+++ 002addtwonumbers.py
@@ -8,14 +8,32 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 """
-class Sulution:
-    def addTwoNumbers(self, l1, l2):
+
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    def addTwoNumbers(l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-        print(l1.next());
+        carry = 0;
+        res = n = ListNode(0);
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next;
+            if l2:
+                carry += l2.val;
+                l2 = l2.next;
+            carry, val = divmod(carry, 10)
+            n.next = n = ListNode(val);
+        return res.next;
 
-test1 = "(2 -> 4 -> 3)"
-print(test1.next)
+test1 = ListNode[2, 4, 3]
+test2 = ListNode[5, 6, 4]
+print(Solution.addTwoNumbers(test1, test2))
