3c32883bbaf1c3a6943870861f82c456816235e2
hp04301986 <hp393747035@126.com>
2018-01-18 15:01:38 +0800

    Given a roman numeral, convert it to an integer.

--- 013romanToInt.py
+++ 013romanToInt.py
@@ -1,6 +1,10 @@
 """
 Given a roman numeral, convert it to an integer.
 Input is guaranteed to be within the range from 1 to 3999.
+相同的数字连写，所表示的数等于这些数字相加得到的数，如 Ⅲ=3；
+小的数字在大的数字的右边，所表示的数等于这些数字相加得到的数，如 Ⅷ=8、Ⅻ=12；
+小的数字（限于 Ⅰ、X 和 C）在大的数字的左边，所表示的数等于大数减小数得到的数，如 Ⅳ=4、Ⅸ=9；
+在一个数的上面画一条横线，表示这个数增值 1,000 倍
 """
 class Solution:
     def romanToInt(self, s):
@@ -14,3 +18,5 @@ class Solution:
             result = result - dic[c] if dic[c] < dic[p] else result + dic[c]
             p = c
         return result
+
+print(Solution.romanToInt(Solution, "MCMXCVI"))
