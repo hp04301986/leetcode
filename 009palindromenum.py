6211794882bce7c3bdefd748fcbe5adf805cab8a
hp04301986 <hp393747035@126.com>
2018-01-18 13:45:31 +0800

    Given a string, find the length of the longest substring without repeating characters.

---
+++ 009palindromenum.py
@@ -0,0 +1,16 @@
+"""
+Determine whether an integer is a palindrome. Do this without extra space.
+click to show spoilers.
+Some hints:
+Could negative integers be palindromes? (ie, -1)
+If you are thinking of converting the integer to string, note the restriction of using extra space.
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+There is a more generic way of solving this problem.
+"""
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return False if x < 0 else str(x)[::-1] == str(x)
