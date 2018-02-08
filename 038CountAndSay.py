0f93b1994588f09dbbda9bffbb4b5e2fceec25a4
hp04301986 <hp393747035@126.com>
2018-02-08 16:33:51 +0800

    The count-and-say sequence is the sequence of integers beginning as follows: 1, 11, 21, 1211, 111221, ...

---
+++ 038CountAndSay.py
@@ -0,0 +1,44 @@
+"""
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth term of the count-and-say sequence.
+Note: Each term of the sequence of integers will be represented as a string.
+Example 1:
+Input: 1
+Output: "1"
+Example 2:
+Input: 4
+Output: "1211"
+"""
+class Solution(object):
+    def countAndSay(n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        s = '1'
+        if n > 1:
+            for _ in range(n-1):
+                count = 1
+                temp = []
+                for index in range(0, len(s) - 1):
+                    if s[index] == s[index + 1]:
+                        count += 1
+                    else:
+                        temp.append(str(count))
+                        temp.append(s[index])
+                        count = 1
+                temp.append(str(count))
+                temp.append(s[-1])
+                s = ''.join(temp)
+        return s
+
+    print(countAndSay(6))
+
