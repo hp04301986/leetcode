d6e338d1b0ec2f8040aeb0b59c317f0da18548a8
hp04301986 <hp393747035@126.com>
2018-02-28 11:14:13 +0800

    Given an integer n, return the number of trailing zeroes in n!.

---
+++ 172FactorialTZero.py
@@ -0,0 +1,19 @@
+"""
+Given an integer n, return the number of trailing zeroes in n!.
+Note: Your solution should be in logarithmic time complexity.
+"""
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        r = 0
+        while n > 0:
+            n //= 5
+            print(n)
+            r += n
+        return r
+
+s = Solution()
+print(s.trailingZeroes(5))
